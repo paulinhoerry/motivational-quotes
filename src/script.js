47c6cd3bd51cca8439278c58b9d6d5d12408561d
Paulo Rodrigues <paulinhoerry@gmail.com>
2016-12-20 16:00:09 -0300

    feat(date): add date component

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
-var getQuote = () => { 
-	return quotes[parseInt(Math.random() * quotes.length)];
+var getQuote = () => {
+    return quotes[parseInt(Math.random() * quotes.length)];
 }
 
 const mark = `
@@ -9,6 +9,18 @@ const mark = `
 	</article>
 `;
 
-
-const article = document.getElementsByClassName('article')[0];
+const article = document.getElementsByClassName('quote')[0];
 article.innerHTML = mark;
+
+
+const date = new Date(Date.now());
+const time = date.toLocaleTimeString().slice(0, 5);
+const day = date.toLocaleDateString().slice(0, 5);
+const dateTag = document.getElementsByClassName('date')[0];
+
+const timeMark = `
+	<div>${time}</div>
+	<div>${day}</div>
+`;
+
+dateTag.innerHTML = timeMark;
